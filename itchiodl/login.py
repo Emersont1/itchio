277d97b543b811ca4292aaba5c16de868bbc848d
Emersont1 <Emersont1@users.noreply.github.com>
2022-03-09 14:04:16 +0000

    autopep8 action fixes

--- itchiodl/login.py
+++ itchiodl/login.py
@@ -3,30 +3,37 @@ import json
 from bs4 import BeautifulSoup as soup
 import requests
 
+
 def LoginWeb(user, password):
-        session = requests.Session()
-        
-        # GET the page first so we have a valid CSRF token value
-        login1 = session.get("https://itch.io/login")
-        s =  soup(login1.text, "html.parser")
-        csrf_token = s.find("input", {"name": "csrf_token"})["value"]
-        
-        # Now POST the login
-        r = session.post("https://itch.io/login", {"username": user, "password":password, "csrf_token": csrf_token})
+    session = requests.Session()
 
-        if r.status_code != 200:
-            raise RuntimeError
+    # GET the page first so we have a valid CSRF token value
+    login1 = session.get("https://itch.io/login")
+    s = soup(login1.text, "html.parser")
+    csrf_token = s.find("input", {"name": "csrf_token"})["value"]
 
-        return session
+    # Now POST the login
+    r = session.post("https://itch.io/login",
+                     {"username": user,
+                      "password": password,
+                      "csrf_token": csrf_token})
+
+    if r.status_code != 200:
+        raise RuntimeError
+
+    return session
+
 
 def LoginAPI(user, password):
-    r = requests.post("https://api.itch.io/login", {"username": user, "password":password, "source": "desktop"})
+    r = requests.post("https://api.itch.io/login",
+                      {"username": user,
+                       "password": password,
+                       "source": "desktop"})
     if r.status_code != 200:
         raise RuntimeError
     t = json.loads(r.text)
-    
+
     if not t["success"]:
         raise RuntimeError
-    
+
     return t["key"]["key"]
-    
