36498a88b49734cc97746e4aa6d252d295c18751
Peter Taylor <pmy@pt3.uk>
2022-03-09 14:05:55 +0000

    Added CIs (And applied Autopep8 fixes) (#18)
    * Added CIs
    
    * autopep8 action fixes (#19)
    
    Co-authored-by: Emersont1 <Emersont1@users.noreply.github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Emersont1 <Emersont1@users.noreply.github.com>

--- itchiodl/bundle.py
+++ itchiodl/bundle.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup as soup
 
+
 class Bundle:
     def __init__(self, login, url):
         self.login = login
         self.url = url
-    
+
     def load_games(self):
         i = 1
-        
+
         r = self.login.get(self.url)
         s = soup(r.text, "html.parser")
         pages = int(s.select("span.pager_label a")[-1].text)
@@ -18,7 +19,8 @@ class Bundle:
                 print(f"Processing Page {i} of {pages}")
 
     def load_game(self, i):
-        # Load 1 game. This will refresh the game afterwards, as the csrf token will update
+        # Load 1 game. This will refresh the game afterwards, as the csrf token
+        # will update
         r = self.login.get(f"{self.url}?page={i}")
         s = soup(r.text, "html.parser")
         for g in s.select("div.game_row"):
@@ -26,14 +28,16 @@ class Bundle:
             if f := g.find("form"):
                 print(f"Processing {name}")
 
-                game_id = f.find("input", {"name":"game_id"})["value"]
-                csrf_token = f.find("input", {"name":"csrf_token"})["value"]
+                game_id = f.find("input", {"name": "game_id"})["value"]
+                csrf_token = f.find("input", {"name": "csrf_token"})["value"]
 
-                data = {"action":"claim", "game_id":game_id, "csrf_token":csrf_token}
+                data = {
+                    "action": "claim",
+                    "game_id": game_id,
+                    "csrf_token": csrf_token}
 
                 r = self.login.post(f"{self.url}?page={i}", data=data)
                 return False
-            #else:
+            # else:
             #    print(f"Skipping {name} - Already in Library")
         return True
-
--- itchiodl/bundle_tool/__main__.py
+++ itchiodl/bundle_tool/__main__.py
@@ -1,15 +1,17 @@
 from getpass import getpass
 import itchiodl
 
+
 def main():
     user = input("Username: ")
     password = getpass("Password: ")
-    
+
     l = itchiodl.LoginWeb(user, password)
 
     url = input("Bundle URL: ")
     b = itchiodl.Bundle(l, url)
     b.load_games()
+
 
 if __name__ == "__main__":
     main()
