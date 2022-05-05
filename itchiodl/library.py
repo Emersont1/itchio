286ee7818a663f7cfcd651a0497bc80451d4e6df
Peter Taylor <me@et1.uk>
2022-05-05 11:11:26 +0100

    linting

--- itchiodl/library.py
+++ itchiodl/library.py
@@ -1,22 +1,25 @@
-import requests
 import json
 from concurrent.futures import ThreadPoolExecutor
+import requests
 
 from itchiodl.game import Game
 
 
 class Library:
+    """Representation of a user's game library"""
+
     def __init__(self, login, jobs=4):
         self.login = login
         self.games = []
         self.jobs = jobs
 
     def load_game_page(self, page):
+        """Load a page of games via the API"""
         print("Loading page", page)
         r = requests.get(
             f"https://api.itch.io/profile/owned-keys?page={page}",
-            headers={
-                "Authorization": self.login})
+            headers={"Authorization": self.login},
+        )
         j = json.loads(r.text)
 
         for s in j["owned_keys"]:
@@ -25,6 +28,7 @@ class Library:
         return len(j["owned_keys"])
 
     def load_games(self):
+        """Load all games in the library via the API"""
         page = 1
         while True:
             n = self.load_game_page(page)
@@ -33,6 +37,10 @@ class Library:
             page += 1
 
     def download_library(self, platform=None):
+        """Download all games in the library"""
         with ThreadPoolExecutor(max_workers=self.jobs) as executor:
-            def dl(g): return g.download(self.login, platform)
+
+            def dl(g):
+                return g.download(self.login, platform)
+
             executor.map(dl, self.games)
